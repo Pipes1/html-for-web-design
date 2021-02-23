ae1ab84b1fd3d11f83a6738b0bd5c179378fe4b8
Pipes1 <nolanjohnclark@gmail.com>
2021-02-23 15:18:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var x = document.getElementById("myBtn");
+var tin = document.getElementById("obama");
+function obamna(){
+  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+for(var a = ''; a.length = 40;) a += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"[(Math.random() * 60) | 0]
+  document.getElementById("obamna").innerHTML = a;
+}
