3ff65bbfa1761b44048ba306c6665c20026be196
Pipes <nolanjohnclark@gmail.com>
2021-02-23 15:37:26 +0000

    jnhnjhnjhjmk

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 var x = document.getElementById("myBtn");
+ document.getElementById("obama").setAttribute('.value','My default value');
 var tin = document.getElementById("obama");
 function obamna(){
   Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-for(var a = ''; a.length = 40;) a += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"[(Math.random() * 60) | 0]
+
+for(var a = ''; a.length < "tin";) a += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz"[(Math.random() * 60) | 0]
   document.getElementById("obamna").innerHTML = a;
 }
